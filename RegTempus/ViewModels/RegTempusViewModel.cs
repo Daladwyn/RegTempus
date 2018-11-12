29a7919e876039d5dcb286352d59f5697be8e7c5
Christian Levin <christian.johansson@gmail.com>
2018-11-12 16:23:35 +0100

    presentation view model is created.

---
+++ RegTempus/ViewModels/RegTempusViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegTempus.ViewModels
+{
+    public class RegTempusViewModel
+    {
+
+        [MaxLength(10)]
+        public string Month { get; set; }
+
+        [Range(1,31)]
+        public int Day { get; set; }
+
+        public DateTime TimeStart { get; set; }
+
+        public DateTime TimeStop { get; set; }
+
+        public DateTime TimeBreak { get; set; }
+    }
+}
