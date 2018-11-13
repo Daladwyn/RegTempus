9928dac508884e6d372c88ee76961cf72e582a89
Christian Levin <christian.johansson@gmail.com>
2018-11-14 00:10:27 +0100

    Added code to figure out the objectId of the authorized user.

--- RegTempus/Controllers/HomeController.cs
+++ RegTempus/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RegTempus.Models;
 using RegTempus.ViewModels;
 
 namespace RegTempus.Controllers
@@ -25,6 +26,23 @@ namespace RegTempus.Controllers
         [HttpPost]
         public IActionResult StartTime(int userId)
         {
+            Registrator registrator = new Registrator();
+            
+            if (User.Identity.IsAuthenticated)
+            {
+                foreach (var Identity in User.Identities)
+                {
+                    foreach (var claim in Identity.Claims)
+                    {
+                        if (claim.Type == "http://schemas.microsoft.com/identity/claims/objectidentifier")
+                        {
+                            registrator.UserId = claim.Value;
+                        }
+                    }
+                }
+            }
+ 
+           
             return View();
         }
 
