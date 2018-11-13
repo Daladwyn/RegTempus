8d0ff6382b899bff08cc721ee158df0ecb14fad9
Christian Levin <christian.johansson@gmail.com>
2018-11-13 09:42:25 +0100

    Added a Registration view model and changed name on other view to better show its purpose.

--- RegTempus/ViewModels/PresentRegisteredTimeViewModel.cs
+++ RegTempus/ViewModels/PresentRegisteredTimeViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RegTempus.ViewModels
 {
-    public class RegTempusViewModel
+    public class PresentRegisteredTimeViewModel
     {
 
         [MaxLength(10)]
